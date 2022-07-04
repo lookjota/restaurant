3810d99511098e327cc25617488e2e9589532865
lookjota <lookjota@gmail.com>
2022-07-04 00:17:22 -0300

    Start project navbar complete

---
+++ src/container/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { images, SubHeading } from '../../components'
+
+import './Header.css';
+
+const Header = () => (
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="chase the new flavour" />
+      <h1 className="app__header-h1">The key to Fine Dining</h1>
+    </div>
+
+    <div className="app__wrapper_img">
+
+    </div>
+  </div>
+);
+
+export default Header;
